336e7991297e487aa45ff0428f6810b74785acc6
Timoteo <timoteohss@gmail.com>
2019-08-05 18:39:21 -0300

    capitulo4

---
+++ sintaxe-variaveis-e-fluxo/testaCondicionais2.dart
@@ -0,0 +1,24 @@
+void main() {
+  print("Testando condicionais");
+
+  int idade = 17;
+  
+  // true significa verdadeiro
+  // false significa falso
+
+  bool maior_idade = idade >= 18;
+  bool acompanhado = false;
+
+  print(maior_idade);
+
+  if(maior_idade) {
+    print("Você pode entrar!");
+  }
+  else {
+    if(acompanhado) {
+      print("Você é menor de idade, mas está acompanhado, pode entrar!");
+    } else {
+      print("Você não pode entrar!");
+    }
+  }
+}
