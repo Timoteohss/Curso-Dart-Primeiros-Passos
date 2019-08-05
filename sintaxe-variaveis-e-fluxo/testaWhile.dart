cd38c0f6efa6b06e0f0796d683c20e2e28193338
Timoteo <timoteohss@gmail.com>
2019-08-05 18:40:53 -0300

    capitulo5

---
+++ sintaxe-variaveis-e-fluxo/testaWhile.dart
@@ -0,0 +1,13 @@
+void main() {
+  int contador = 11;
+
+  // != significa diferente
+  // ++ significa +1
+
+  while(contador <= 10){
+    print("O valor do contador é $contador");
+    contador++;
+  }
+
+  print("Finalizando o programa");
+}
