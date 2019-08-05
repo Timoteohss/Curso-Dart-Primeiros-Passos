70ed52fb519baca050c202219195849dae37a0ea
Timoteo <timoteohss@gmail.com>
2019-08-05 18:39:12 -0300

    capitulo4

---
+++ sintaxe-variaveis-e-fluxo/testaCondicionais.dart
@@ -0,0 +1,17 @@
+void main() {
+  print("Testando condicionais");
+  int idade = 25;
+
+  // < significa menor que
+  // > significa maior que
+  // == significa igual a
+  // <= significa menor igual
+  // >= significa maior igual
+
+  if(idade >= 18) {
+    print("Você é maior de idade, pode entrar!");
+  }
+  else {
+    print("Você é menor de idade, não pode entrar!");
+  }
+}
